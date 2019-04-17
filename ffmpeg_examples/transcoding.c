e2a8d8f91760fcc93790c575cfd922aaad556028
wangrl <wangrl2016@gmail.com>
2019-04-18 00:22:38 +0800

    Writing transocoding example.

--- ffmpeg_examples/transcoding.c
+++ ffmpeg_examples/transcoding.c
@@ -12,7 +12,8 @@
 /**
  * API example for demuxing, decoding, filtering, encoding and muxing
  */
-
+static AVFormatContext *ifmt_ctx;
+static AVFormatContext *ofmt_ctx;
 
 typedef struct FilteringContext {
     AVFilterContext *buffersink_ctx;
@@ -20,12 +21,92 @@ typedef struct FilteringContext {
     AVFilterGraph *filter_graph;
 } FilteringContext;
 
-static int open_input_file(const char *filename) {
+typedef struct StreamContext {
+    AVCodecContext *dec_ctx;
+    AVCodecContext *enc_ctx;
+} StreamContext;
 
+static StreamContext *stream_ctx;
+
+
+static int open_input_file(const char *filename) {
+    int ret;
+    unsigned int i;
+
+    ifmt_ctx = NULL;
+    if ((ret = avformat_open_input(&ifmt_ctx, filename, NULL, NULL)) < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Cannot open input file\n");
+        return ret;
+    }
+
+    if ((ret = avformat_find_stream_info(ifmt_ctx, NULL)) < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Cannot find stream information\n");
+        return ret;
+    }
+
+    // 有多个stream
+    stream_ctx = av_mallocz_array(ifmt_ctx->nb_streams, sizeof(*stream_ctx));
+    if (!stream_ctx)
+        return AVERROR(ENOMEM);
+
+    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+        AVStream *stream = ifmt_ctx->streams[i];
+        AVCodec *dec = avcodec_find_decoder(stream->codecpar->codec_id);
+        AVCodecContext *codec_ctx;
+        if (!dec) {
+            av_log(NULL, AV_LOG_ERROR, "Failed to find decoder for stream #%u\n", i);
+            return AVERROR_DECODER_NOT_FOUND;
+        }
+        codec_ctx = avcodec_alloc_context3(dec);
+        if (!codec_ctx) {
+            av_log(NULL, AV_LOG_ERROR, "Failed to allocate the decoder context for stream #%u\n", i);
+            return AVERROR(ENOMEM);
+        }
+        ret = avcodec_parameters_to_context(codec_ctx, stream->codecpar);
+        if (ret < 0) {
+            av_log(NULL, AV_LOG_ERROR, "Failed to copy decoder parameters to input decoder context "
+                                       "for stream #%u\n", i);
+            return ret;
+        }
+        /* Reencode video & audio and remux subtitles etc. */
+        if (codec_ctx->codec_type == AVMEDIA_TYPE_VIDEO
+                || codec_ctx->codec_type == AVMEDIA_TYPE_AUDIO) {
+            if (codec_ctx->codec_type == AVMEDIA_TYPE_VIDEO)
+                codec_ctx->framerate = av_guess_frame_rate(ifmt_ctx, stream, NULL);
+            /* Open decoder */
+            ret = avcodec_open2(codec_ctx, dec, NULL);
+            if (ret < 0) {
+                av_log(NULL, AV_LOG_ERROR, "Failed to open decoder for stream #%u\n", i);
+                return ret;
+            }
+        }
+        stream_ctx[i].dec_ctx = codec_ctx;
+    }
+    av_dump_format(ifmt_ctx, 0, filename, 0);
+    return 0;
 }
 
 static int open_output_file(const char *filename) {
+    AVStream *out_stream;
+    AVStream *in_stream;
+    AVCodecContext *dec_ctx, *enc_ctx;
+    AVCodec *encoder;
+    int ret;
+    unsigned int i;
 
+    ofmt_ctx = NULL;
+    avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, filename);
+    if (!ofmt_ctx) {
+        av_log(NULL, AV_LOG_ERROR, "Could not create output context\n");
+        return AVERROR_UNKNOWN;
+    }
+
+    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+        out_stream = avformat_new_stream(ofmt_ctx, NULL);
+        if (!out_stream) {
+
+        }
+    }
 }
 
 static int init_filter(FilteringContext *fctx, AVCodecContext *dec_ctx,
@@ -33,7 +114,7 @@ static int init_filter(FilteringContext *fctx, AVCodecContext *dec_ctx,
 
 }
 
-static void init_filters(void) {
+static int init_filters(void) {
 
 }
 
@@ -66,5 +147,7 @@ int main(int argc, char *argv[]) {
     if ((ret = init_filters()) < 0)
         goto end;
 
+
     end:
+    av_packet_unref(&packet);
 }
